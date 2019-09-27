92dda9dab1bdf2fbd629ce3c871e0f9c6e04957e
Ryan <ryanberrypi@protonmail.com>
2019-09-27 09:26:57 -0500

    Need to find how to check if a float is a digit

--- change.c
+++ change.c
@@ -1,42 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
 
 float getCost(void);
 float getPay(void);
-//void calcMoney();
+void calcMoney();
 //void calcChange(remaining);
 //void exitRun();
 //void clearScreen();
 
 float getCost(void){
     float cost;
-    printf("Please enter the cost of the item: ");
-    scanf("%f",&cost);
-    while(cost <= 0){
-        printf("Cost must be greater than zero. Enter a new cost: ");
+    while(1){
+        printf("Please enter the cost: ");
         scanf("%f",&cost);
+
+        if (!isdigit(cost)){
+            printf("Value must be a number.");
+            continue;
+        }
+
+        else if (cost <= 0){
+            printf("Cost must be greater than zero.");
+            continue;
+        }
+
+        else 
+            break;
+        
     }
+
     return cost;
 }
 
 float getPay(void){
     float pay;
-    printf("Please enter the cost of the item: ");
-    scanf("%f",&pay);
-    while(pay <= 0){
-        printf("Cost must be greater than zero. Enter a new cost: ");
+    while(1){
+        printf("Please enter the cost: ");
         scanf("%f",&pay);
+
+        if (!isdigit(pay)){
+            printf("Value must be a number.");
+            continue;
+        }
+
+        else if (pay <= 0){
+            printf("Cost must be greater than zero.");
+            continue;
+        }
+
+        else 
+            break;
+        
     }
+
     return pay;
 }
 
-void main(){
+void calcMoney(){
     float cost = getCost();
     float pay = getPay();
-    float diff = cost - pay;
-    printf("%.2f\n",diff);
-    printf("Test");
-    int choice;
-    scanf("%d",&choice);
+
+    if (cost > pay){
+        float diff = cost - pay;
+        printf("Not enough money to cover the cost. %.2f is still needed to cover the cost.",diff);
+    }
+    else if (cost == pay){
+        printf("Cost and pay are even. No change needed.");
+    }
+
+    else {
+        printf("Calculate how much change is needed.");
+    }
+
+}
+
+void main(){
+    calcMoney();
 }
