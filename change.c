d6920cc3b4c497b917cde122ae52a600595b77d1
Ryan McChesney <ryanberrypi@Ryans-MacBook-Pro.local>
2019-09-30 12:49:36 -0500

    Rewrote the program. Need to fix the arrays.

--- change.c
+++ change.c
@@ -3,82 +3,94 @@
 #include <math.h>
 #include <ctype.h>
 
-float getCost(void);
-float getPay(void);
+float getCost();
+float getPay();
 void calcMoney();
-//void calcChange(remaining);
+void calcChange(float remaining);
 //void exitRun();
 //void clearScreen();
 
-float getCost(void){
+float getCost(){
     float cost;
-    while(1){
-        printf("Please enter the cost: ");
+    while (1){
+        printf("Cost: ");
         scanf("%f",&cost);
         printf("\n");
-
-        if (!isdigit(cost)){
-            printf("Value must be a number.");
-            continue;
-        }
-
-        else if (cost <= 0){
+        
+        if (cost <= 0){
             printf("Cost must be greater than zero.");
             continue;
         }
 
-        else 
+        else
             break;
-        
     }
-
     return cost;
 }
 
-
-
-float getPay(void){
+float getPay(){
     float pay;
-    while(1){
-        printf("Please enter the cost: ");
+    while (1){
+        printf("Given: ");
         scanf("%f",&pay);
-
-        if (!isdigit(pay)){
-            printf("Value must be a number.");
-            continue;
-        }
-
-        else if (pay <= 0){
-            printf("Cost must be greater than zero.");
-            continue;
-        }
-
-        else 
-            break;
+        printf("\n");
         
-    }
+        if (pay <= 0){
+            printf("Amount given must be greater than zero.");
+            continue;
+        }
 
+        else
+            break;
+    }
     return pay;
 }
 
 void calcMoney(){
     float cost = getCost();
-    float pay = getPay();
+    float paid = getPay();
 
-    if (cost > pay){
-        float diff = cost - pay;
-        printf("Not enough money to cover the cost. %.2f is still needed to cover the cost.",diff);
+    if (cost > paid){
+        float diff = cost - paid;
+        printf("Not enough paid. %f still owed.\n",diff);
     }
-    else if (cost == pay){
-        printf("Cost and pay are even. No change needed.");
+
+    else if (cost == paid){
+        printf("Cost and paid are even. No change needed.\n");
     }
 
     else {
-        printf("Calculate how much change is needed.");
+        float chDiff = paid - cost;
+        printf("---Change---\n");
+        calcChange(chDiff);
     }
 
 }
 
-void main(){
+void calcChange(float remaining){
+
+    int v;
+    float C_VALUE[] = {100,50,20,10,5,1,0.025,0.10,0.05,0.01};
+    char C_WORD[] = {"$100 Dollar Bills","$50 Dollar Bills","$20 Dollar Bills",
+    "$10 Dollar Bills","$5 Dollar Bills","$1 Dollar Bills","Quarters","Dimes",
+    "Nickels","Pennies"};
+
+    for (v=0 ; v<C_VALUE; v++){
+        int C_COUNT = 0;
+        while (1){
+            if (remaining - C_VALUE[v] >= 0){
+                remaining -= C_VALUE[v];
+                C_COUNT += 1;
+                continue;
+            }
+
+            else {
+                printf("%d %s\n",C_COUNT,C_WORD[v]);
+            }
+        }
+    }
+}
+
+int main(){
     calcMoney();
 }
